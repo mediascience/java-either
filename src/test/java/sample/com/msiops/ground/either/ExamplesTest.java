36724b20d6ca54a15000becbc89b829c2a744ea1
Greg Wiley <aztec.rex@jammm.com>
2014-11-08 17:19:31 -0800

    [cleanup] run examples as test

---
+++ src/test/java/sample/com/msiops/ground/either/ExamplesTest.java
@@ -0,0 +1,12 @@
+package sample.com.msiops.ground.either;
+
+import org.junit.Test;
+
+public class ExamplesTest {
+
+    @Test
+    public void testExamples() {
+        RunExamples.runSilentyl();
+    }
+
+}
